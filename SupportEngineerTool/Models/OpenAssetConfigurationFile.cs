910307a873c21283dbbbd1476dfae1d9f7a5f7e1
Fausto Payano <fausto.payano@axomic.com>
2016-09-16 17:55:00 -0400

    Syncnig One last file.

---
+++ SupportEngineerTool/Models/OpenAssetConfigurationFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportEngineerTool.Models {
+    class OpenAssetConfigurationFile {
+        public string InstallPath { get; set; }
+        public string Data_Path { get; set; }
+        public string[] ApacheListeningPorts { get; set; }
+    }
+}
