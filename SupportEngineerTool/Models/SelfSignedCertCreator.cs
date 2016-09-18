061f52bfc511c7277f3fd14f2d27cf4a48ccb8b1
jestrella1984 <je2451@columbia.edu>
2016-09-17 23:05:15 -0400

    added user control
    added apache user control and began writing logic to create self signed
    certificates.

---
+++ SupportEngineerTool/Models/SelfSignedCertCreator.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics;
+
+namespace SupportEngineerTool.Models
+{
+    class SelfSignedCertCreator
+    {
+        
+        //check openasset conf for any ssl configuration
+        public static void CheckForExistingOpenAssetConf()
+        {
+            bool response = false;
+            string oaConfFile = "@C:\\Apache\\conf\\OpenAsset.conf";
+
+            response = File.Exists(oaConfFile) ? true : false;
+
+            //TO DO: Check if ssl is configured in old apache: Parse and grab paths to current cert and keys
+
+           
+        }
+        
+     
+        
+        public static void generateSelfSignedSSLCert()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(@"c:\\Apache\\bin\\openssl.exe", "req - newkey rsa: 2048 - nodes - keyout server.key - x509 - days 730 -out server.cert");
+            Process.Start(startInfo);
+        }
+        
+    }
+}
