dccf8f03a6d7295e7e89e50efb25e05a7c995195
jestrella1984 <je2451@columbia.edu>
2016-10-07 00:51:23 -0400

    Added Drag and Drop Functionality.
    Need to add a text block to house the file path along with some try
    catch blocks. Need robust error detection. Also need to plug the
    messages to Serilog.

--- SupportEngineerTool/CustomControls/Apache.xaml.cs
+++ SupportEngineerTool/CustomControls/Apache.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,41 @@ namespace SupportEngineerTool.CustomControls
         public Apache()
         {
             InitializeComponent();
+        }
+
+        private void Card_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true) == true)
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            
+        }
+
+        private void Card_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true) == true)
+            {   
+            
+                string filename = ((string[])e.Data.GetData(DataFormats.FileDrop,true)).First();
+                string extension = System.IO.Path.GetExtension(filename);
+                if (extension.Equals(".pfx") || extension.Equals(".pkcs"))
+                {
+                    //This if just to test if the file path came in.
+                    //I still need to add the path to another textbox so the user can see the 
+                    //file that will be processed
+                    MessageBox.Show(filename);
+                }
+                else
+                {
+                    MessageBox.Show("Error: " + extension + " is not a valid file type.");
+                }
+                
+            }
         }
     }
 }
