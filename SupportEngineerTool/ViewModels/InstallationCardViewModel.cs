af7b291d8ce50bac73d7e40b906c789ed84f8fa4
Fausto-Payano-Axomic <fausto.payano@axomic.com>
2017-03-22 17:38:30 -0400

    Revert "Latest version commit prior to restore"
    This reverts commit 82fd0803a0e291b3b9aa12c864ed00b6cd788765.

--- SupportEngineerTool/ViewModels/InstallationCardViewModel.cs
+++ SupportEngineerTool/ViewModels/InstallationCardViewModel.cs
@@ -20,10 +20,8 @@ namespace SupportEngineerTool.ViewModels {
         private string _sslCertAU;
         private string _sslCertFile;
         private string _sslCertKey;
-        private string _versionNumber;
 
         public ICommand RefreshCommand { get; set; }
- 
 
         public InstallationCardViewModel() {
             installInfo = new InstallInformationModel();
@@ -38,8 +36,6 @@ namespace SupportEngineerTool.ViewModels {
 
         private void LoadCommands() {
             RefreshCommand = new CustomCommand(RefreshInformation, CanRefreshInformation);
-            VersionNumber = GetPublishedVersion();
-
         }
         private void RefreshInformation(object obj) {
             installInfo.RefreshConfigFile();
@@ -97,13 +93,6 @@ namespace SupportEngineerTool.ViewModels {
             }
         }
 
-        public string VersionNumber {
-            get { return _versionNumber; }
-            set {
-                _versionNumber = value;
-                NotifyPropertyChanged();
-            }
-        }
         public void UpdateConfigInformation() {
             try {
                 installInfo.RefreshConfigFile();
@@ -140,13 +129,6 @@ namespace SupportEngineerTool.ViewModels {
 
         public event PropertyChangedEventHandler PropertyChanged;
         #endregion
-        private string GetPublishedVersion() {
-            if (System.Deployment.Application.ApplicationDeployment.IsNetworkDeployed) {
-                return System.Deployment.Application.ApplicationDeployment.CurrentDeployment.
-                    CurrentVersion.ToString();
-            }
-            return "No Version information Available.";
-        }
 
     }
 }
